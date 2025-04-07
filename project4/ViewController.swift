e7b011a3200caed11bc13d1216884eb8b1a02ae2
godgiftedboy <rajprajapati12321@gmail.com>
2025-04-07 13:40:31 +0545

    chore: added rightBarButtonItem to navigate some pages

--- project4/ViewController.swift
+++ project4/ViewController.swift
@@ -21,9 +21,43 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         let url = URL(string: "https://www.hackingwithswift.com")!
         webview.load(URLRequest(url: url))
         webview.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc func openTapped (){
+        //message is nil because this alert doesnt need one.
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet);
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage));
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage));
+        
+        //no handler here as it will just cancel the displayed alert box.
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel));
+        
+        //is imported for iPad
+        //It tells iOs where action sheets should be anchored.
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem;
+        present(ac,animated: true);
+        
+    }
+    
+    func openPage(action: UIAlertAction){
+        guard let actionTitle = action.title
+        else{
+            return;
+        }
+        guard let url = URL(string: "https://" + actionTitle) else{return}
+        webview.load(URLRequest(url: url))
+        webview.allowsBackForwardNavigationGestures = true
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webview.title;
     }
 
 
